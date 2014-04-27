4901692c5aeb62332cd981dd583c9dd4d9bb79c1
Chien-Te Liu <ufoderek@gmail.com>
2014-04-28 01:35:56 +0800

    repo-cleanup.sh: Detect and use rcmd.sh for buildfarm

--- scripts/repo-cleanup.sh
+++ scripts/repo-cleanup.sh
@@ -1,5 +1,19 @@
-repo forall -c "git reflog expire --expire=now --all"
-repo forall -c "git fsck --full --unreachable"
-repo forall -c "git repack -A -d"
-repo forall -c "git gc --aggressive --prune=now"
+function is_buildfarm {
+    local HOSTNAME=`hostname`
+    if [[ "$HOSTNAME" = ws* ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
+if is_buildfarm; then
+    prefix="rcmd.sh"
+else
+    prefix=""
+fi
+
+repo forall -c "$prefix git reflog expire --expire=now --all"
+repo forall -c "$prefix git fsck --full --unreachable"
+repo forall -c "$prefix git repack -A -d"
+repo forall -c "$prefix git gc --aggressive --prune=now"
