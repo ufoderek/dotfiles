8285c4bd6fa9f81b4cfde92dc881c7102f897d81
Chien-Te Liu <ufoderek@gmail.com>
2014-05-05 10:26:26 +0800

    repo-reset.sh: Detect and use rcmd.sh for buildfarm

--- scripts/repo-reset.sh
+++ scripts/repo-reset.sh
@@ -1,3 +1,18 @@
+function is_buildfarm {
+    local HOSTNAME=`hostname`
+    if [[ "$HOSTNAME" = ws* ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if is_buildfarm; then
+    prefix="rcmd.sh"
+else
+    prefix=""
+fi
+
 if [ -z "$1" ]; then
     echo "`basename $0` base_branch_name {branch_name_to_del} {sync}"
 else
@@ -5,17 +20,11 @@ else
         echo "target directory: $i"
         if [[ "$2" = "sync" || "$3" = "sync" ]]; then
             echo "syncing ..."
-            (cd $i && repo sync .) > /dev/null 2>&1
+            (cd $i && $prefix repo sync .) > /dev/null 2>&1
         fi
-        (cd $i && git reset --hard && git checkout -b _dev $1) > /dev/null 2>&1
-        (cd $i && git branch -D dev; git branch -D dev2; git branch -D dev3; git branch -D dev4) > /dev/null 2>&1
-        (cd $i && git branch -D tb; git branch -D tb2; git branch -D tb3; git branch -D tb4) > /dev/null 2>&1
-        if [[ -n "$2" && "$2" != "sync" ]]; then
-            (cd $i && git branch -D $2) > /dev/null 2>&1
-        fi
-        if [[ -n "$3" && "$3" != "sync" ]]; then
-            (cd $i && git branch -D $3) > /dev/null 2>&1
-        fi
-        (cd $i && git checkout -b dev $1 && git branch -D _dev && git reset --hard)
+        (cd $i && $prefix git reset --hard && $prefix git checkout -b _dev $1) > /dev/null 2>&1
+        (cd $i && $prefix git branch -D dev; $prefix git branch -D dev2; $prefix git branch -D dev3; $prefix git branch -D dev4) > /dev/null 2>&1
+        (cd $i && $prefix git branch -D tb; $prefix git branch -D tb2; $prefix git branch -D tb3; $prefix git branch -D tb4) > /dev/null 2>&1
+        (cd $i && $prefix git checkout -b dev $1 && $prefix git branch -D _dev && $prefix git reset --hard)
     done
 fi
