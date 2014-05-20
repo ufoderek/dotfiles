cf27198da9ab40e04f34c9cc6d28fd098b90b0a9
Chien-Te Liu <ufoderek@gmail.com>
2014-05-20 13:21:33 +0800

    Improve pullmtk.sh and add pullaee.sh

--- scripts/pullaee.sh
+++ scripts/pullaee.sh
@@ -3,7 +3,7 @@ if [ ! -d $LOG_DIR ]; then
     mkdir -p $LOG_DIR
 fi
 mkdir -p $LOG_DIR/aee_exp
-mkdir -p $LOG_DIR/mtklog
+mkdir -p $LOG_DIR/mtklog/aee_exp
 adb pull /data/aee_exp $LOG_DIR/aee_exp
-adb pull /sdcard/mtklog $LOG_DIR/mtklog
+adb pull /sdcard/mtklog/aee_exp $LOG_DIR/mtklog/aee_exp
 echo "destination: $LOG_DIR"
