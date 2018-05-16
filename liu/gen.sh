2fd0b873c1f41ce7289c20d2c26c8636dd6a5d6b
Chien-Te Liu <ufoderek@gmail.com>
2018-05-16 12:25:05 +0800

    Revert "Add ibus custom table"
    This reverts commit 2515e76463091c4af0458df4a9ee2c71e009b7d2.

--- liu/gen.sh
+++
@@ -1,5 +0,0 @@
-ibus-table-createdb -d -s l_table.txt -n liu.db
-sudo cp liu.db /usr/share/ibus-table/tables/
-sudo cp l_icon.png /usr/share/ibus-table/icons/liu.png
-sudo chmod 644 /usr/share/ibus-table/tables/liu.db
-sudo chmod 644 /usr/share/ibus-table/icons/liu.png
