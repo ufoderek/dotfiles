206d7bac13ca1eb42ecab62394b6eb7a56cbb84b
Chien-Te Liu <ufoderek@gmail.com>
2018-10-10 00:56:48 +0800

    Add fc_cleanup.sh to trim down Fedora

---
+++ scripts/fc_cleanup.sh
@@ -0,0 +1,25 @@
+sudo dnf erase simple-scan
+sudo dnf erase evolution
+sudo dnf erase PackageKit
+sudo dnf erase gnome-maps
+sudo dnf erase gnome-user-docs
+sudo dnf erase gnome-user-share
+sudo dnf erase cheese 
+
+# List system services
+# sudo systemctl -t service
+sudo systemctl disable libvirtd.service
+
+# Show running user services.
+# $ systemctl --user status
+# Show all user units and their status.
+# $ systemctl --user list-unit-files
+systemctl --user mask evolution-addressbook-factory.service
+systemctl --user mask evolution-calendar-factory.service
+systemctl --user mask evolution-source-registry.service
+
+systemctl --user stop tracker-store.service
+rm -rf ~/.cache/tracker/
+systemctl --user mask tracker-store.service
+
+
