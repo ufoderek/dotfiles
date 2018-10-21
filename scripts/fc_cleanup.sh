e93902af8610ad8bad9cb5b6b5f2928dd02d850b
Chien-Te Liu <ufoderek@gmail.com>
2018-10-22 00:54:33 +0800

    Update fc- scripts

--- scripts/fc_cleanup.sh
+++ scripts/fc_cleanup.sh
@@ -1,14 +1,15 @@
 sudo dnf erase simple-scan
 sudo dnf erase evolution
-sudo dnf erase PackageKit
+# sudo dnf erase PackageKit
 sudo dnf erase gnome-maps
 sudo dnf erase gnome-user-docs
 sudo dnf erase gnome-user-share
 sudo dnf erase cheese 
+sudo dnf erase vlgothic-fonts
 
 # List system services
 # sudo systemctl -t service
-sudo systemctl disable libvirtd.service
+# sudo systemctl disable libvirtd.service
 
 # Show running user services.
 # $ systemctl --user status
@@ -21,5 +22,3 @@ systemctl --user mask evolution-source-registry.service
 systemctl --user stop tracker-store.service
 rm -rf ~/.cache/tracker/
 systemctl --user mask tracker-store.service
-
-
---
+++ scripts/fc_install.sh
@@ -0,0 +1,8 @@
+sudo dnf install vim git tmux vlc pcmanx-gtk2 calibre HandBrake gnome-tweaks
+sudo dnf install fcitx fcitx-table fcitx-table-extra fcitx-configtool fcitx-chewing
+sudo dnf group install "C Development Tools and Libraries"
+sudo dnf install google-chrome-stable
+sudo dnf install exfat-utils fuse-exfat
+sudo dnf install p7zip zip unzip unrar
+sudo dnf install gimp
+sudo dnf install VirtualBox virtualbox-guest-additions-ogl
