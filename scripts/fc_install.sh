f6582492bfb4f27292dd6c6684b50b4eb160ffa6
Chien-Te Liu <ufoderek@gmail.com>
2018-11-03 11:57:34 +0800

    Re-organize fc_install.sh

--- scripts/fc_install.sh
+++ scripts/fc_install.sh
@@ -1,8 +1,8 @@
-sudo dnf install vim git tmux vlc pcmanx-gtk2 calibre HandBrake gnome-tweaks
+sudo dnf install vim git tmux vlc pcmanx-gtk2 calibre HandBrake gnome-tweaks tlp
 sudo dnf install fcitx fcitx-table fcitx-table-extra fcitx-configtool fcitx-chewing
-sudo dnf group install "C Development Tools and Libraries"
-sudo dnf install google-chrome-stable
-sudo dnf install exfat-utils fuse-exfat
-sudo dnf install p7zip zip unzip unrar
-sudo dnf install gimp
+sudo dnf install adobe-source-han-sans-tw-fonts adobe-source-han-serif-tw-fonts
+sudo dnf install exfat-utils fuse-exfat p7zip zip unzip unrar
 sudo dnf install VirtualBox virtualbox-guest-additions-ogl
+sudo dnf install google-chrome-stable
+sudo dnf install gimp
+sudo dnf group install "C Development Tools and Libraries"
