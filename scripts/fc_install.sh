1dfe003a39da46f8523b0b6db0cf248e63d1e62b
Chien-Te Liu <ufoderek@gmail.com>
2018-11-14 07:35:53 +0800

    Update fc_install.sh

--- scripts/fc_install.sh
+++ scripts/fc_install.sh
@@ -1,8 +1,16 @@
-sudo dnf install vim git tmux vlc pcmanx-gtk2 calibre HandBrake gnome-tweaks tlp
-sudo dnf install fcitx fcitx-table fcitx-table-extra fcitx-configtool fcitx-chewing
-sudo dnf install adobe-source-han-sans-tw-fonts adobe-source-han-serif-tw-fonts
+sudo dnf install vim git tmux vlc pcmanx-gtk2 gnome-tweaks trash-cli
 sudo dnf install exfat-utils fuse-exfat p7zip zip unzip unrar
+sudo dnf install fcitx fcitx-table fcitx-table-extra fcitx-configtool fcitx-chewing
+sudo dnf group install "C Development Tools and Libraries"
+
+sudo dnf install adobe-source-han-sans-tw-fonts adobe-source-han-serif-tw-fonts
 sudo dnf install VirtualBox virtualbox-guest-additions-ogl
 sudo dnf install google-chrome-stable
-sudo dnf install gimp
-sudo dnf group install "C Development Tools and Libraries"
+sudo dnf install gimp calibre HandBrake
+
+# Laptops
+# sudo dnf install tlp
+# Desktops (Intel)
+# sudo dnf install kernel-tools
+# sudo systemctl enable cpupower.service
+# sudo systemctl start cpupower.service
