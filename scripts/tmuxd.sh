49546f2f646ab53a88d64ce20931ddc77d2b8d09
ChienTe Liu <ChienTe_Liu@novatek.com.tw>
2019-05-03 17:50:18 +0800

    tmuxd.sh: Cleanup

--- scripts/tmuxd.sh
+++ scripts/tmuxd.sh
@@ -1,33 +1,25 @@
 # if $TMUX is set the tmux is currently running
 if [ -n "${TMUX}" ]; then
-    echo "tmux is already running, now exit."
+    echo "tmux is already running, abort..."
     exit 0;
 fi
 
 # the name of your primary tmux session
 if [ -n "$1" ]; then
-    SESSION=$1
+    SESSION_NAME=$1
 else
-    SESSION=$USER
+    SESSION_NAME=$USER
 fi
 
-# if the session is already running, just attach to it.
-tmux has-session -t $SESSION
+tmux has-session -t $SESSION_NAME > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "tmux: attach to session: $SESSION"
-    tmux -2 detach-client -a -s $SESSION #detach all other clients
-	tmux -2 attach -t $SESSION
-    echo "tmux: detatch from session: $SESSION"
-	exit 0;
+    # if the session is already running
+    echo "tmux: detach other clients: $SESSION_NAME"
+    tmux -2 detach-client -a -s $SESSION_NAME
 else
-    # create a new session, named $SESSION, and detach from it
-    tmux -2 new-session -d -s $SESSION
-
-    # all done. select starting window and get to work
-    # you may need to cycle through windows and type in passwords
-    # if you don't use ssh keys
-    #tmux select-window -t $SESSION:0
-    echo "tmux: attach to new session: $SESSION"
-    tmux -2 attach -t $SESSION
-    echo "tmux: detatch from new session: $SESSION"
+    # create a new session
+    echo "tmux: create a new session: $SESSION_NAME"
+    tmux -2 new-session -d -s $SESSION_NAME -c ~/wk
 fi
+echo "tmux: attach to session: $SESSION_NAME"
+tmux -2 attach -t $SESSION_NAME
