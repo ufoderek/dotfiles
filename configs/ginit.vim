d232d72ea117648491d4d1b1fe14cc5932591afb
Chien-Te Liu <ufoderek@gmail.com>
2019-08-05 23:04:20 +0800

    Support neovim

---
+++ configs/ginit.vim
@@ -0,0 +1,12 @@
+set termguicolors
+set guifont=Consolas:h16 "nvim
+
+let g:neosolarized_contrast = "high"
+let g:neosolarized_visibility = "normal"
+let g:neosolarized_vertSplitBgTrans = 1
+let g:neosolarized_bold = 1
+let g:neosolarized_underline = 1
+let g:neosolarized_italic = 0
+
+set background=dark
+colorscheme NeoSolarized
--- scripts/pack-df.sh
+++ scripts/pack-df.sh
@@ -1,5 +1,11 @@
 MY_DATE=`date +%Y-%m%d-%H%M%S`
+
 rm -rf ~/dotfiles/vim-snapshot
 cp -r ~/.vim ~/dotfiles/vim-snapshot
 rm -rf `find ~/dotfiles/vim-snapshot -name \.git`
+
+rm -rf ~/dotfiles/nvim-snapshot
+cp -r ~/.config/nvim ~/dotfiles/nvim-snapshot
+rm -rf `find ~/dotfiles/nvim-snapshot -name \.git`
+
 tar -C ~ -czf ~/dotfiles-$MY_DATE.tar.xz dotfiles
