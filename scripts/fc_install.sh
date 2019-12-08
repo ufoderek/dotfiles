6865d5c6fcd175eb00ef48f852b6fce0965a520e
Chien-Te Liu <ufoderek@gmail.com>
2019-12-09 02:12:04 +0800

    Update fc_install.sh

--- scripts/fc_install.sh
+++ scripts/fc_install.sh
@@ -1,19 +1,24 @@
-sudo dnf install vim neovim git tmux vlc pcmanx-gtk2 gnome-tweaks trash-cli smartmontools exfat-utils fuse-exfat p7zip zip unzip unrar
-sudo dnf group install "C Development Tools and Libraries"
+sudo dnf install vim neovim git tmux vlc gnome-tweaks trash-cli smartmontools \
+exfat-utils fuse-exfat \
+p7zip zip unzip unrar \
+gimp calibre HandBrake google-chrome-stable \
+cjkuni-uming-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-tw-fonts \
+dconf-editor VirtualBox
 
-sudo dnf install gimp calibre HandBrake cjkuni-uming-fonts google-chrome-stable adobe-source-han-sans-tw-fonts adobe-source-han-serif-tw-fonts dconf-editor VirtualBox
+# sudo dnf install pcmanx-gtk2
+sudo dnf group install "C Development Tools and Libraries"
+sudo dnf group install Multimedia
+
 
 # Dropbox
-sudo dnf install nautilus-dropbox gnome-shell-extension-appindicator
+# sudo dnf install nautilus-dropbox gnome-shell-extension-appindicator
 # dropbox start # and follow the suggestion to fix /etc/sysctl.conf
 
 # Laptops
 # sudo dnf install tlp
 
-# Desktops (Intel)
-# sudo dnf install kernel-tools
-# sudo systemctl enable cpupower.service
-# sudo systemctl start cpupower.service
+# ibus-rime
+sudo dnf install ibus-rime
 
 # fcitx
 # sudo dnf install fcitx fcitx-table fcitx-table-extra fcitx-configtool fcitx-chewing
