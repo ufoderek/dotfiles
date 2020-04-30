cb2d392e8ae9b964d62b98fed87060c8617df104
Chien-Te Liu <ufoderek@protonmail.com>
2020-05-01 01:43:22 +0800

    Add vocal to fc_install.sh

--- scripts/fc_install.sh
+++ scripts/fc_install.sh
@@ -1,16 +1,16 @@
 sudo dnf install neovim git tmux mpv vlc gnome-tweaks trash-cli ibus-rime \
 exfat-utils fuse-exfat \
+blivet-gui \
 p7zip zip unzip unrar \
 gimp calibre VirtualBox \
 cjkuni-uming-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-tw-fonts \
-dconf-editor chromium-browser-privacy gnome-music
+chromium-browser-privacy gnome-music vocal
+# dconf-editor
 # pavucontrol
 # HandBrake
-# chrome
 
 # sudo dnf install pcmanx-gtk2
 sudo dnf group install "C Development Tools and Libraries"
-# sudo dnf group install Multimedia
 # sudo dnf groupupdate Multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 
 # Laptops
