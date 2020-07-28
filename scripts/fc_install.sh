b1cda45df3dbcfe1296cca9ade5b69f9997edd4e
Chien-Te Liu <ufoderek@protonmail.com>
2020-07-29 03:29:40 +0800

    Update fc_install.sh

--- scripts/fc_install.sh
+++ scripts/fc_install.sh
@@ -1,21 +1,25 @@
-sudo dnf install neovim git tmux mpv vlc gnome-tweaks trash-cli ibus-rime \
+sudo dnf install -y neovim git tmux mpv gnome-tweaks trash-cli ibus-rime \
 exfat-utils fuse-exfat \
 blivet-gui \
 p7zip zip unzip unrar \
 gimp calibre VirtualBox \
 cjkuni-uming-fonts \
-chromium-browser-privacy gnome-music vocal
+chromium-browser-privacy \
+thunderbird-wayland nextcloud-client
+#gnome-music vocal
 
-sudo dnf install ccid pcsc-tools # for EasyATM GT2
+#sudo dnf -y install ccid pcsc-tools # for EasyATM GT2
 
 # adobe-source-han-sans-tw-fonts adobe-source-han-serif-tw-fonts
 # dconf-editor
 # pavucontrol
 # HandBrake
+# vlc
 
-# sudo dnf install pcmanx-gtk2
-sudo dnf group install "C Development Tools and Libraries"
-# sudo dnf groupupdate Multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+# sudo dnf install -y pcmanx-gtk2
+# sudo dnf group install -y "C Development Tools and Libraries"
+# sudo dnf groupupdate Multimedia -y --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate Multimedia
 
 # Laptops
-sudo dnf install tlp smartmontools
+sudo dnf install -y tlp smartmontools
