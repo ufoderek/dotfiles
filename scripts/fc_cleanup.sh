e592f66ea3abeb7650322db1520a4f4e38930753
Chien-Te Liu <ufoderek@protonmail.com>
2020-08-01 03:11:18 +0800

    Add fc_cleanup.sh

---
+++ scripts/fc_cleanup.sh
@@ -0,0 +1,7 @@
+sudo dnf remove gnome-boxes gnome-calendar gnome-dictionary gnome-documents gnome-maps gnome-weather gnome-photos
+
+sudo dnf remove gnome-online-miners gnome-video-effects
+
+sudo dnf remove gnome-getting-started-docs gnome-initial-setup gnome-user-docs gnome-user-share
+
+sudo dnf remove gfbgraph cheese simple-scan
