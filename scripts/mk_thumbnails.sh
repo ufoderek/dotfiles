711866478fb5a249480c6e3613d8e80935fad7ab
Chien-Te Liu <ufoderek@protonmail.com>
2020-09-14 03:41:21 +0800

    Update mk_thumbnails.sh

--- scripts/mk_thumbnails.sh
+++ scripts/mk_thumbnails.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-src_dir=/home/ufoderek/ufoderek/Nextcloud/Photos
-dst_dir=/home/ufoderek/ufoderek/Nextcloud/Thumbs
+base_dir=/mnt/ufoderek/Nextcloud
+src_dir=Photos
+dst_dir=Thumbs
 
 params="-sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -resize 25%"
 
-for src in `find $src_dir -type f -iname *.jpg -print`
-do
-	rel_src=$(realpath --relative-to="$src_dir" "$src")
-	dst="$dst_dir/$rel_src"
-	mkdir -p $(dirname "$dst")
-	convert "$src" $params "$dst"
+(cd $base_dir/$src_dir && find . -type f -iname *.jpg -print0) |
+while IFS= read -r -d '' jpg_path; do
+	echo "$jpg_path"
+	(cd $base_dir && mkdir -p $(dirname "$dst_dir/$jpg_path") && convert "$src_dir/$jpg_path" $params "$dst_dir/$jpg_path")
 done
