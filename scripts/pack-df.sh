7718187a4703ca78df6a9faf1599f1c703c61d3c
Chien-Te Liu <ufoderek@protonmail.com>
2021-08-02 23:42:06 +0800

    pack-df: Remove vim-related features

--- scripts/pack-df.sh
+++ scripts/pack-df.sh
@@ -1,9 +1,5 @@
 MY_DATE=`date +%Y-%m%d-%H%M%S`
 
-rm -rf ~/dotfiles/vim-snapshot
-cp -r ~/.vim ~/dotfiles/vim-snapshot
-rm -rf `find ~/dotfiles/vim-snapshot -name \.git`
-
 rm -rf ~/dotfiles/nvim-snapshot
 cp -r ~/.config/nvim ~/dotfiles/nvim-snapshot
 rm -rf `find ~/dotfiles/nvim-snapshot -name \.git`
