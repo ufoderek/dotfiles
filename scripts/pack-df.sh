4e345f26282fa79d74abbc200d433f1650433949
Chien-Te Liu <ufoderek@protonmail.com>
2021-08-23 04:21:20 +0800

    Add upnvim

--- scripts/pack-df.sh
+++ scripts/pack-df.sh
@@ -1,8 +1,8 @@
 MY_DATE=`date +%Y-%m%d-%H%M%S`
 
 rm -rf ~/dotfiles/nvim-snapshot
-cp -r ~/.config/nvim ~/dotfiles/nvim-snapshot
-rm -rf `find ~/dotfiles/nvim-snapshot -name \.git`
+cp -r ~/.config/nvim ~/dotfiles/nvim-snapshot/
+cp -r ~/.local/share/nvim/site ~/dotfiles/nvim-snapshot/
 
 XZ_OPT="-9" tar -C ~ -cJf ~/dotfiles-$MY_DATE.tar.xz dotfiles
 (cd ~ && split ~/dotfiles-$MY_DATE.tar.xz -b 2M dotfiles-$MY_DATE.tar.xz.split-)
