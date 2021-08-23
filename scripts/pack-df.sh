5fe4aa22571963f00c904cfc8e07f0701ba6d47c
Chien-Te Liu <ufoderek@protonmail.com>
2021-08-23 22:59:13 +0800

    Fix pack-df.sh and upnvim

--- scripts/pack-df.sh
+++ scripts/pack-df.sh
@@ -1,8 +1,11 @@
 MY_DATE=`date +%Y-%m%d-%H%M%S`
 
-rm -rf ~/dotfiles/nvim-snapshot
-cp -r ~/.config/nvim ~/dotfiles/nvim-snapshot/
-cp -r ~/.local/share/nvim/site ~/dotfiles/nvim-snapshot/
+SNAPSHOT=$HOME/dotfiles/nvim-snapshot
+
+rm -rf $SNAPSHOT
+mkdir -p $SNAPSHOT
+cp -r ~/.config/nvim $SNAPSHOT/
+cp -r ~/.local/share/nvim/site $SNAPSHOT/
 
 XZ_OPT="-9" tar -C ~ -cJf ~/dotfiles-$MY_DATE.tar.xz dotfiles
 (cd ~ && split ~/dotfiles-$MY_DATE.tar.xz -b 2M dotfiles-$MY_DATE.tar.xz.split-)
