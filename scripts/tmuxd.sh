8af905f6a670ca10cc131ff406133558283c67f1
Chien-Te Liu <ufoderek@protonmail.com>
2021-08-26 00:07:23 +0800

    tmuxd.sh: Update default session config

--- scripts/tmuxd.sh
+++ scripts/tmuxd.sh
@@ -8,18 +8,18 @@ fi
 if [ -n "$1" ]; then
     SESSION_NAME=$1
 else
-    SESSION_NAME=$USER
+    SESSION_NAME=kernel
 fi
 
 tmux has-session -t $SESSION_NAME > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     # if the session is already running
     echo "tmux: detach other clients: $SESSION_NAME"
-    tmux -2 detach-client -a -s $SESSION_NAME
+    tmux detach-client -a -s $SESSION_NAME
 else
     # create a new session
     echo "tmux: create a new session: $SESSION_NAME"
-    tmux -2 new-session -d -s $SESSION_NAME -c ~/wk
+    tmux new-session -d -s $SESSION_NAME -c ~/wk
 fi
 echo "tmux: attach to session: $SESSION_NAME"
-tmux -2 attach -t $SESSION_NAME
+tmux attach -t $SESSION_NAME
